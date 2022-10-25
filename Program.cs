c2d9ad169d0af20c0ac3028afbb00fde4e17eddf
Francis Rodrigues Lopes <fro@phatconsulting.de>
2022-10-25 15:16:35 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OKAufgabenRechner
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, im OK. Your personal calculator");
+            string arithmeticOperator = Console.ReadLine();
+            if (arithmeticOperator -- "+")
+            {
+               Console.WriteLine("Enter the first and second Number");
+               string numberone - Console.ReadLine()
+               Console.WriteLine($"Number one: {}. Enter Number Two:");
+            }
+
+            Console.ReadKey(); 
+        }
+    }       
+}
